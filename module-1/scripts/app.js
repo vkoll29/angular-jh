0263c88cc9f22f3d4b282018edec70209332e7f8
vkoll29 <awino_g@outlook.com>
2016-10-18 22:38:07 +0300

    Solution to module 1 of AngularJS offered by Johns Hopkins on Coursera

---
+++ module-1/scripts/app.js
@@ -0,0 +1,35 @@
+(function(){
+  'use strict';
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope){
+      $scope.lunchItems = "";
+      $scope.msg = "";
+      $scope.rslt = "default";
+      $scope.color = "black";
+      $scope.checkItems = function(){
+        var noSpace = $scope.lunchItems.trim();
+        var empty = (noSpace === "");
+        if(empty){
+          $scope.msg = "Please enter data first";
+          $scope.rslt = "danger";
+          $scope.color = "red";
+        }
+        else{
+          $scope.rslt = "success";
+          $scope.color = "green"
+          var items = $scope.lunchItems.split(',')
+          if(items.length <4){
+            $scope.msg = "Enjoy";
+          }
+          else{
+            $scope.msg = "Too much";
+          }
+        }
+      }
+
+      console.log($scope.msg);
+    }
+})();
